92c401339697504a6a59eec6e746dced321856a1
Kevin Donkers <kevin.donkers@informaticslab.co.uk>
2020-11-12 18:58:34 +0000

    tidy data script

--- scripts/data_tidy.R
+++ scripts/data_tidy.R
@@ -8,3 +8,21 @@ waste <- read_csv(here::here("data/wastepercapita_UNdata_Export_20201112_0746487
 
 pop <- read_csv(here::here("data/pop_UNdata_Export_20201112_075133467.csv"))
 psw <- read_csv(here::here("data/popservedwaste_UNdata_Export_20201112_074710796.csv"))
+
+# Filter pop to contain total national population of both sexes
+pop_tot = filter(pop, Sex=="Both Sexes" & Area=="Total") %>%
+  + select(c("Country or Area", "Year", "Value"))
+
+# Wrangle into one df
+data <- select(waste, c("Country or Area", "Year", "Value")) %>%
+  rename("waste"="Value") %>%
+  filter(!is.na(Year)) %>%
+  left_join(select(gdp, -Item)) %>%
+  rename("gdp_pc"="Value") %>%
+  left_join(pop_tot) %>%
+  rename("pop"="Value")
+
+# Waste in 1000s tonnes -> tonnes per capita
+data <- mutate(data, waste=waste*1000/pop) %>%
+  rename("waste_pc"="waste")
+
